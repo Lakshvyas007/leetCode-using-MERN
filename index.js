529681caad7258c83df1af51addc7e2e78d76601
Lakshya Vyas <lakshyavyas987@gmail.com>
2023-08-10 15:00:11 +0530

    Submit-Submissions-Done

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ const port = 3001
 
 const USERS = [];
 
-const QUESTIONS = [{
+const QUESTIONS = [
+    {
     title: "Two states",
     description: "Given an array , return the maximum of the array?",
     testCases: [{
         input: "[1,2,3,4,5]",
         output: "5"
     }]
-}];
+},
+{
+    title: "One state",
+    description: "Given an array , return the maximum of the array",
+    testCases: [{
+        input: "[1,2,3,4,5]",
+        output: "5",
+    }]
+}]
 
 app.use(express.json());
 
-const SUBMISSION = [
+const SUBMISSIONS = [
 
 ]
 
@@ -25,26 +34,20 @@ app.use((req, res, next) => {
 });
 
 app.post('/signup', function (req, res) {
-
     // Add logic to decode body
     // body should have email and password
     const { email, password } = req.body;
-
     // console.log(req.body);
-
     //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
     const existingUser = USERS.find(user => user.email === email);
     if (existingUser) {
         return res.status(400).json({ message: 'User with this email already exists' });
     }
-
-    const newUser = { email, password };
+     const newUser = { email, password };
     USERS.push(newUser);
-
     // return back 200 status code to the client
     res.status(200).json({ message: 'User successfully signed up' })
 })
-
 
 app.get('/users', (req, res) => {
     res.json({ users: USERS });
@@ -54,15 +57,12 @@ app.post('/login', function (req, res) {
     // Add logic to decode body
     // body should have email and password
     const { email, password } = req.body;
-
     // Check if the user with the given email exists in the USERS array
     const user = USERS.find(user => user.email === email);
     // Also ensure that the password is the same
     if (!user || user.password !== password) {
-      return  res.status(401).json({ message: `Authentication failed` });
+        return res.status(401).json({ message: `Authentication failed` });
     }
-
-
     // If the password is the same, return back 200 status code to the client
     const token = 'randomToken';
     // Also send back a token (any random string will do for now)
@@ -71,21 +71,34 @@ app.post('/login', function (req, res) {
 })
 
 app.get('/questions', function (req, res) {
-
     //return the user all the questions in the QUESTIONS array
-    res.send("Hello World from route 3!")
+    res.json({questions:QUESTIONS});
 })
 
-app.get("/submissions", function (req, res) {
-    // return the users submissions for this problem
-    res.send("Hello World from route 4!")
+// app.get("/submissions", function (req, res) {
+//     // return the users submissions for this problem
+//     res.send("Hello World from route 4!")
+// });
+
+app.post("/submit", function (req, res) {
+    const { userId, problemId, code } = req.body;
+
+    // Create a new submission object and store it in SUBMISSIONS array
+    const newSubmission = { userId, problemId, code };
+    SUBMISSIONS.push(newSubmission);
+
+    res.json({ message: "Submission successful", submission: newSubmission });
 });
 
+app.get("/submissions", function (req, res) {
+    // Return the user's submissions for this problem
+    const userId = req.query.userId; // Assuming you pass the userId as a query parameter
+    const problemId = req.query.problemId; // Assuming you pass the problemId as a query parameter
 
-app.post("/submissions", function (req, res) {
-    // let the user submit a problem, randomly accept or reject the solution
-    // Store the submission in the SUBMISSION array above
-    res.send("Hello World from route 4!")
+    // Filter submissions based on userId and problemId
+    const userSubmissions = SUBMISSIONS.filter(submission => submission.userId === userId && submission.problemId === problemId);
+
+    res.json({ submissions: userSubmissions });
 });
 
 // leaving as hard todos
